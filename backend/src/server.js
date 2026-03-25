7b27f15e94d2e0544d839cc4b4df16682941ac57
Prat-375 <prateek.nayak50@gmail.com>
2026-03-25 22:33:39 +0100

    changed server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,6 +39,10 @@ if (process.env.NODE_ENV === "production") {
 
 
 connectDB().then(() => {
+    app.get("/", (req, res) => {
+    res.send("ThinkBoard API is running");
+});
+
     app.listen(PORT, () => {
     console.log("Server is running on PORT:", PORT);
 });
